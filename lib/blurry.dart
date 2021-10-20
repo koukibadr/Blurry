b086b15cf02a19f74d0efbed446e37223498b8d8
Kouki Badr <badreddinekouki1998@gmail.com>
2021-10-20 04:27:32 +0100

    add example app project

--- lib/blurry.dart
+++ lib/blurry.dart
@@ -1,7 +1,25 @@
-library blurry;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Blurry extends StatelessWidget {
+  const Blurry({ Key? key }) : super(key: key);
+
+  void show(BuildContext context){
+    showDialog(context: context, builder: (context) {
+      return AlertDialog(
+        content: this,
+        contentPadding: const EdgeInsets.all(0),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white
+      ),
+    );
+  }
 }
