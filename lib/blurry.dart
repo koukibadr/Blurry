aae5014edbbdecbe2988fc3f1ae52dbf67a941f1
Kouki Badr <badreddinekouki1998@gmail.com>
2021-10-20 04:46:34 +0100

    create base blurry widget

--- lib/blurry.dart
+++ lib/blurry.dart
@@ -1,25 +1,30 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Blurry extends StatelessWidget {
-  const Blurry({ Key? key }) : super(key: key);
+  const Blurry({Key? key}) : super(key: key);
 
-  void show(BuildContext context){
-    showDialog(context: context, builder: (context) {
-      return AlertDialog(
-        content: this,
-        contentPadding: const EdgeInsets.all(0),
-      );
-    });
+  void show(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: this,
+            contentPadding: const EdgeInsets.all(0),
+            backgroundColor: Colors.white.withOpacity(0),
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
+      height: 300,
+      width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white
-      ),
+          borderRadius: BorderRadius.circular(10),
+          color: (const Color(0xFFDFDFDF)).withOpacity(0.85)),
     );
   }
 }
